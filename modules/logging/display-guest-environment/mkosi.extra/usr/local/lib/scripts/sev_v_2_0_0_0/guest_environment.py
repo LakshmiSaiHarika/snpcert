a4d8910d8b04fc5bb6048e570cd3779f67cbb645
Harika Nittala <lnittala@amd.com>
2025-08-20 05:09:07 -0700

    feat: Extract SNP Guest Environment details inside the guest
    Signed-off-by: Harika Nittala <lnittala@amd.com>

---
+++ modules/logging/display-guest-environment/mkosi.extra/usr/local/lib/scripts/sev_v_2_0_0_0/guest_environment.py
@@ -0,0 +1,36 @@
+import sys
+import subprocess
+import re
+from guest_os_package import GuestOSPackage
+
+class GuestEnvironment:
+    """ Show guest environment details """
+
+    def get_guest_os_version(self):
+        os_version_command = f"hostnamectl | grep \"Operating System\" | cut -d':' -f2"
+        # os_version_command = f"cat /etc/os-release | grep PRETTY_NAME"
+        command = subprocess.run(os_version_command, shell=True, check=True, text=True, capture_output=True)
+        os_version = command.stdout.strip()
+
+        # os_version = os_version.replace("PRETTY_NAME=", "").replace("\"", "").strip()
+        guest_os_version = "Guest Operating System: " + os_version
+        return guest_os_version
+
+    def get_guest_os_id(self):
+        os_version_command = f"grep \'^ID=\' /etc/os-release | cut -d\'=\' -f2"
+        os_version_command = os_version_command.strip()
+        command = subprocess.run(os_version_command, shell=True, check=True, text=True, capture_output=True)
+        guest_os_id = command.stdout.strip()
+        return guest_os_id
+
+# Get installed guest package versions
+    def get_guest_kernel_version(self):
+        os_id = self.get_guest_os_id()
+        kernel_pkg_name = GuestOSPackage.guest_kernel.get(os_id)
+        kernel_command = subprocess.run(["/usr/local/lib/scripts/sev_v_2_0_0_0/package_version.sh", kernel_pkg_name], capture_output=True, text=True, check=True)
+        guest_kernel_version = kernel_command.stdout.strip()
+        return "Guest Kernel Version: " + guest_kernel_version
+
+ge = GuestEnvironment()
+print(ge.get_guest_os_version())
+print(ge.get_guest_kernel_version())
---
+++ modules/logging/display-guest-environment/mkosi.extra/usr/local/lib/scripts/sev_v_2_0_0_0/guest_os_package.py
@@ -0,0 +1,10 @@
+import sys
+import subprocess
+import re
+
+class GuestOSPackage:
+    """ Maps guest packages/components to their respective OS package names  """
+
+    guest_kernel={}
+    guest_kernel["fedora"]="kernel"
+    guest_kernel["ubuntu"]="linux-gcp"
