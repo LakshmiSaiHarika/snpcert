7a1dc5047fccf49f38340272cac8779c289fc2ec
Harika Nittala <lnittala@amd.com>
2025-08-28 14:58:48 -0700

    feat: Add support for Centos 10
    Signed-off-by: Harika Nittala <lnittala@amd.com>

--- modules/logging/generate-sev-report/mkosi.extra/usr/local/lib/scripts/sev_report/test_environment/sev_version_2_0_0_0/host_environment/host_os_package.py
+++ modules/logging/generate-sev-report/mkosi.extra/usr/local/lib/scripts/sev_report/test_environment/sev_version_2_0_0_0/host_environment/host_os_package.py
@@ -9,13 +9,16 @@ class HostOSPackage:
     qemu["fedora"]="qemu"
     qemu["ubuntu"]="qemu-system"
     qemu["debian"]="qemu-system"
+    qemu["centos"]="qemu-kvm-core"
 
     ovmf={}
     ovmf["fedora"]="edk2-ovmf"
     ovmf["ubuntu"]="ovmf"
     ovmf["debian"]="ovmf"
+    ovmf["centos"]="edk2-ovmf"
 
     host_kernel={}
     host_kernel["fedora"]="kernel"
     host_kernel["ubuntu"]="linux-image-virtual"
     host_kernel["debian"]="linux-image-amd64"
+    host_kernel["centos"]="kernel"
